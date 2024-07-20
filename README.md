61c5737c8aced38a740f7bf4ff756c957c551f07
Alex Guerra <alexander.a.guerra@gmail.com>
2024-07-19 21:36:59 -0500

    Started documentation

---
+++ README.md
@@ -0,0 +1,11 @@
+# Playground
+
+## How to Load An Image
+
+1.
+
+## Data
+
+### Images
+
+I am trying to use `.jpg` as my file format.
--- sketch.js
+++ sketch.js
@@ -1,3 +1,7 @@
+/// On the first frame, the setup() function is called once and draw() is called repeatedly.
+/// The draw() function is called 60 times per second by default.
+let tick = 0
+
 function setup() {
     createCanvas(400, 400)
 }
