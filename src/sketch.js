4db6d20d9c82e781a82840166fe64a4e25395e5e
Alex Guerra <alexander.a.guerra@gmail.com>
2024-07-20 12:56:52 -0500

    started the ascii project

--- src/sketch.js
+++ src/sketch.js
@@ -3,13 +3,24 @@
 let clock = {
     ticks: 0,
 }
+let myImage
+
+const density =
+    '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,"^`\'.'
+
+function preload() {
+    // Load any assets here
+    myImage = loadImage('data/images/dragon.jpg')
+}
 
 function setup() {
     createCanvas(windowWidth, windowHeight)
     angleMode(DEGREES)
+    print(density)
 }
 
 function draw() {
     background(0)
+    image(myImage, 0, 0, myImage.windowHeight, myImage.windowWidth)
     clock.ticks += 1
 }
