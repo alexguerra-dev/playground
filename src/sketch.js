002f1d05c04f00989a1c04aeb6d37042cfcf13d5
Alex Guerra <alexander.a.guerra@gmail.com>
2024-07-20 10:41:19 -0500

    refactor: Update sketch.js to track ticks with a clock object
    The commit refactors the sketch.js file to introduce a clock object that tracks the number of ticks. This change improves the organization and readability of the code by encapsulating the tick variable within the clock object.

--- src/sketch.js
+++ src/sketch.js
@@ -1,10 +1,19 @@
-let tick = 0
+// This is the main file for the sketch
+
+let clock = {
+    ticks: 0,
+}
 
 function setup() {
     createCanvas(1000, 1000)
+
     print('setup')
 }
 
 function draw() {
     background(20, 255, 60)
+
+    print(clock.ticks)
+
+    clock.ticks += 1
 }
