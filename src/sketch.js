b75dd7266db096a6b88a770beb0af6ac1e571542
Alex Guerra <alexander.a.guerra@gmail.com>
2024-07-20 15:56:04 -0500

    Add some information about making shapes

--- src/sketch.js
+++ src/sketch.js
@@ -6,10 +6,22 @@ let clock = {
 
 function setup() {
     createCanvas(windowWidth, windowHeight)
+
+    // Basic setup for the sketch
     angleMode(DEGREES)
 }
 
 function draw() {
     background(0)
+
+    // Some properties that can be set that effects the drawing.
+    fill(255, 0, 0)
+    stroke(0, 255, 0)
+    strokeWeight(8)
+
+    // Draw a rectangle at the top left corner of the screen.
+    rect(0, 0, 100, 100)
+
+    // Tick the clock
     clock.ticks += 1
 }
