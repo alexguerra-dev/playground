94f7324055cce14d2ebb6d3c55a3c3c10e9d03f3
Alex Guerra <alexander.a.guerra@gmail.com>
2024-07-20 10:26:57 -0500

    refactor: Update index.html and sketch.js
    The commit updates the index.html file to include lazy loading for images, improving page load performance. Additionally, the sketch.js file is modified to import the getFullName function from the utils.js file and print the full name in the setup function. These changes enhance the functionality and performance of the project.

---
+++ src/sketch.js
@@ -0,0 +1,14 @@
+// import getFullName from './utils.js'
+// const tester = require('./utils.js')
+
+let tick = 0
+
+function setup() {
+    createCanvas(1000, 1000)
+    print('setup')
+    print(getFullName('John', 'Doe'))
+}
+
+function draw() {
+    background(20, 255, 60)
+}
---
+++ src/utils.js
@@ -0,0 +1,5 @@
+// utils.js
+function getFullName(firstname, lastName) {
+    console.log('utils.js')
+    return `My fullname is ${firstname} ${lastName}`
+}
